63289d4da1124207d0e1e5bf8a64e0c632db42c7
Emaline <38117483+emaline@users.noreply.github.com>
2020-05-22 19:19:25 -0700

    OurStaff updates
    OurStaff updates

--- src/screens/OurStaff.js
+++ src/screens/OurStaff.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import '../styles/OurStaff.css'
-
+import srpastors from "../images/srpastors";
 
 
 const OurStaff = () => (
-<div className='mainContainer'>
-    <div className='ourstaff'>
-    <h1>About Our Wonderful Staff</h1>
-
-    <p>Sr. Pastors Keith and Dede Nettles</p> 
+    <div className='mainContainer'>
+        <div className='ourstaff'>
+        <h1>About Our Wonderful Staff</h1>
     
-    <p>Associate Pastor Gandalf The Grey</p>
+       <div> 
+        <p>Sr. Pastors Keith and Dede Nettles</p> 
+        <img> src </img>
+       
+       </div>
 
-    <p>Children's Pastor Esme Rider</p>
-
-    <p>Women's Pastor Faye Jackson</p>
-
-    <p>Young Audlts Pastor Ben Courson</p>
-
+        
+        <p>Associate Pastor Gandalf The Grey</p>
+    
+        <p>Children's Pastor Esme Rider</p>
+    
+        <p>Women's Pastor Faye Jackson</p>
+    
+        <p>Young Audlts Pastor Ben Courson</p>
+    
+        </div>
+        
+        <div className='packages'> </div>
     </div>
+    )
     
-    <div className='packages'> </div>
-</div>
-)
-
-export default OurStaff; 
+    
+    export default OurStaff; 
