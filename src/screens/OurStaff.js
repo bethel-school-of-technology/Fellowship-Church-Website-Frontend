0e98a97df3b54b01daff2534da40401cb6f4a425
Emaline <38117483+emaline@users.noreply.github.com>
2020-05-22 20:40:52 -0700

    updated staff page with images

--- src/screens/OurStaff.js
+++ src/screens/OurStaff.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import srpastors from "../images/srpastors";
+import srpastors from "../images/srpastors.jpeg";
+import associate from "../images/associate.jpg";
+import womenspastors from "../images/womenspastors.jpg";
+import youngadultpastors from "../images/youngadultpastors.jpg";
 
 
 const OurStaff = () => (
     <div className='mainContainer'>
         <div className='ourstaff'>
-        <h1>About Our Wonderful Staff</h1>
-    
-       <div> 
-        <p>Sr. Pastors Keith and Dede Nettles</p> 
-        <img> src </img>
-       
-       </div>
+        <h1>Fellowship Church Staff</h1>
+        </div>
+
+        <h2>Senior Pastors</h2>
+        <div className='srpastors'>
+        <p id='nameTag'>Sr. Pastors Keith and Dede Nettles</p>
+        <img className="srpastors img-fluid" src={srpastors} alt="Senior-Pastors"></img>
+         <p id="description">Pastor Keith Nettles has been the Senior Pastor of Fellowship Church for 10 years.</p>
+        </div>
+
+        <h2>Associate Pastors</h2>
+        <div className='associatepastors'>
+        <p id='nameTag'>Associate Pastors Jon and Tannith Brown</p>
+        <img className="associate img-fluid" src={associate} alt="Associate-Pastors"></img>
+        <p id="description">Assocaite Pastor Jon Brown recieved his Masters degree from Yale Divinity and has worked under Sr. Pastor Nettles for 3 years.</p>
+        </div>
+
+        <h2>Women's Ministry Pastor</h2>
+        <div className='womenspastors'>
+        <p id='nameTag'>Women's Pastors Dede Nettles, and Faye Connaway</p>
+        <img className="womenspastors img-fluid" src={womenspastors} alt="Womens-Pastors"></img>
+        <p id="description">Co-Pastors Faye Connaway, and Dede Nettles head the women's ministry.</p>
+        </div>
+
+        <h2>Children and Young Adults Pastors</h2>
+        <div className='youngadultpastors'>
+        <p id='nameTag'>Pastor Eddie Devala and his wife Desi serve our children, youth, and young adults.</p>
+        <img className="youngadultpastors img-fluid" src={youngadultpastors} alt="Youth-Pastors"></img>
+        <p id="description">Pastor Eddie Devala and his wife Desi have served the children, youth, and young adults of fellowship church for 8 years.</p>
+        </div>
 
         
-        <p>Associate Pastor Gandalf The Grey</p>
-    
-        <p>Children's Pastor Esme Rider</p>
-    
-        <p>Women's Pastor Faye Jackson</p>
-    
-        <p>Young Audlts Pastor Ben Courson</p>
-    
-        </div>
-        
-        <div className='packages'> </div>
+
+
     </div>
     )
-    
-    
-    export default OurStaff; 
+
+    export default OurStaff;
