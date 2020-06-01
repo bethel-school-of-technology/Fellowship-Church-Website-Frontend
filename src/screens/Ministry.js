43d3011b060d94433f9ba2b49861d4e836d993c4
SarahToastmaster <53976329+SarahToastmaster@users.noreply.github.com>
2020-05-31 23:33:45 -0500

    updated carousel css

--- src/screens/Ministry.js
+++ src/screens/Ministry.js
@@ -1,111 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/Ministry.scss';
-import ImgComp from './ImgComp';
+
 
 
 function Carousel() {
   const [x, setX] = useState(0);
+  let selectedImage = [
+    <div>
+      <img className="carousel_cell" src="/img/Photo1.jpg" alt="1"/>,
+      <img className="carousel_cell" src="/img/Photo2.jpg" alt="2"/>,
+      <img className="carousel_cell" src="/img/Photo3.jpg" alt="3"/>,
+      <img className="carousel_cell" src="/img/Photo4.jpg" alt="4"/>,
+      <img className="carousel_cell" src="/img/Photo5.jpg" alt="5"/>
+  </div>
   
-  let sliderArr = [
-
-    <ImgComp src="/img/Photo1.jpg" alt="I'm"/>,
-    <ImgComp src="/img/Photo2.jpg" alt="having"/>,
-    <ImgComp src="/img/Photo3.jpg" alt="many"/>,
-    <ImgComp src="/img/Photo4.jpg" alt="issues"/>,
-    <ImgComp src="/img/Photo5.jpg" alt="help!"/>
-    
   ];
+ 
+
+  const prevButton = () => {
+    x=== 0 ? setX(-100 * (selectedImage.length - 1)) : setX(x + 100);
+  };
   
-
-  const goLeft = () => {
-      console.log(x);
-      x=== 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
-  };
-  const goRight = () => {
-      console.log(x);
-    x=== -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+  const nextButton = () => {
+    x=== -100 * (selectedImage.length - 1) ? setX(0) : setX(x - 100)
   };
 
 
-
-return(
-  <div className="carousel">
+return( 
+  
+  <div className="screen" transform = 'translateZ(-137px) rotateY(-72deg)'>
+    <button id="goLeft" onClick={prevButton}>Previous</button>
         
-
-    {sliderArr.map((item, index) => {
-        return(  <div key={index} className="slide" /* style={{transform: translateX('${x}'%)}} */>
-           {item}
-           </div>
-           );
-  
-      })}
-
-
-      <button id="goLeft" onClick={goLeft}>previous button
-      
-      </button>
-      
-      <button id="goRight" onClick={goRight}>next button
-     
-      </button>
-
-
+    <div className="carousel">
+        
+        {selectedImage.map((item, index) => {
+          return( 
+             <div key={index} >
+             {item}
+             </div>
+             );
     
-      </div>
+        })}
+  
+      </div> 
+      <button id="goRight" onClick={nextButton}>Next</button>
+  </div>
 
-);
+  );
 }
-
-
-
-
-
-
-/* 
-const photos = [ 
-    
-
-
-  {name: 'Photo 1',
- url: "https://images.pexels.com/photos/935944/pexels-photo-935944.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
- {name: 'Photo 2',
- url: "/photos/Community_Projectshttps://images.pexels.com/photos/2219024/pexels-photo-2219024.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"},
- {name: 'Photo 3',
- url: "https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
- {name: 'Photo 4',
- url: "https://images.pexels.com/photos/296301/pexels-photo-296301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
- {name: 'Photo 5',
- url: "https://images.pexels.com/photos/576926/pexels-photo-576926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-  ];
-  
-  
-  class Ministry extends React.Component {
-    render() {
-      const settings = {
-        dots: true,
-        fade: true, 
-        infinte: true, 
-        speed: 500,
-        slidesToShow: 1, 
-        arrows: true, 
-        slidesToScroll: 1, 
-        className: "slides"
-      }
-      return (
-        <div className="Ministry">
-          <Slider {...settings}>
-            {photos.map((photo) => {
-              return(
-                <div>
-                  <img width="100%" src={photo.url} alt="photos"/>
-                 
-                </div>
-              );
-            })};
-          </Slider>
-        </div>
-      );
-    }
-  };
- */
 export default Carousel;
